8f1126a3b0d24e4861e27faba79c4432257d7f99
Charles Kelley <c.k.walters@Charless-Mac-mini.local>
2025-06-02 11:11:59 -0400

    (bug) fix harpoon and match harpoon2

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,17 +7,17 @@ return {
     lazy = false,
     priority = 900,
     config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+        local harpoon = require("harpoon")
+        harpoon.setup({})
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-        vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end)
-        vim.keymap.set("n", "<C-5>", function() ui.nav_file(5) end)
-        vim.keymap.set("n", "<C-6>", function() ui.nav_file(6) end)
-        vim.keymap.set("n", "<C-7>", function() ui.nav_file(7) end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
+        vim.keymap.set("n", "<C-6>", function() harpoon:list():select(6) end)
+        vim.keymap.set("n", "<C-7>", function() harpoon:list():select(7) end)
     end,
 }
